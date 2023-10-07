f90705ac42c46bb5c008d819348877161d57a753
phoen-ix <100175489+phoen-ix@users.noreply.github.com>
2023-10-08 00:18:33 +0200

    Add files via upload
    organized file structure, added icons, author

---
+++ data/background.js
@@ -0,0 +1,20 @@
+browser.runtime.onInstalled.addListener(() => {
+  browser.contextMenus.create({
+    id: 'openInMantis',
+    title: 'Open in Mantis',
+    contexts: ['selection']
+  });
+});
+
+browser.contextMenus.onClicked.addListener(async (info) => {
+  if (info.menuItemId === 'openInMantis') {
+    let selectedText = info.selectionText;
+    if (selectedText.startsWith('#')) {
+      selectedText = selectedText.substring(1);
+    }
+    const result = await browser.storage.sync.get(['customUrl']);
+    const fixedUrl = result.customUrl || 'https://example.com/view.php?id=';
+    const completeUrl = `${fixedUrl}${selectedText}`;
+    await browser.tabs.create({ url: completeUrl });
+  }
+});
