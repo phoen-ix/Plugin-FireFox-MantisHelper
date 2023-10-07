ab9788c9cdb97d80e386b206823106e305ae010c
phoen-ix <100175489+phoen-ix@users.noreply.github.com>
2023-10-08 01:32:52 +0200

    Add files via upload

--- data/background.js
+++ data/background.js
@@ -9,12 +9,20 @@ browser.runtime.onInstalled.addListener(() => {
 browser.contextMenus.onClicked.addListener(async (info) => {
   if (info.menuItemId === 'openInMantis') {
     let selectedText = info.selectionText;
-    if (selectedText.startsWith('#')) {
-      selectedText = selectedText.substring(1);
+
+    // Check if selectedText is defined, not just white spaces, and is a number
+    if (selectedText && selectedText.trim() && !isNaN(selectedText.trim())) {
+      if (selectedText.startsWith('#')) {
+        selectedText = selectedText.substring(1);
+      }
+      const result = await browser.storage.sync.get(['customUrl']);
+      const fixedUrl = result.customUrl || 'https://example.com/view.php?id=';
+      const completeUrl = `${fixedUrl}${selectedText}`;
+      await browser.tabs.create({ url: completeUrl });
+    } else {
+      // Open the error page in a new tab
+      await browser.tabs.create({ url: browser.runtime.getURL('data/invalid_selection.html') });
     }
-    const result = await browser.storage.sync.get(['customUrl']);
-    const fixedUrl = result.customUrl || 'https://example.com/view.php?id=';
-    const completeUrl = `${fixedUrl}${selectedText}`;
-    await browser.tabs.create({ url: completeUrl });
   }
 });
+
